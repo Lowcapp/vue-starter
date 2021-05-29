7a91ef8d3e2ee4398cee945fb39dfea98825705e
Pawel Pietraszek <ppietraszek@student.agh.edu.pl>
2021-05-29 15:21:03 +0200

    added LoginForm

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,36 @@
 <template>
   <div>
 	<h1>Witaj w systemie do zapisów na zajęcia</h1>
-  <div v-if="authenticatedEmail != ''">
+    <div v-if="authenticatedEmail != ''">
 	ZALOGOWANY
 	<H4>Jeseś zalogowany jako {{ authenticatedEmail }}</H4>
 	<a @click="logMeOut()">Wyloguj</a>
-  </div>
-  <div v-else>
-	Wpisz login:
-	<input type="text" v-model="email"> <button @click="logMeIn()">Wchodzę</button>
-  </div>
+    </div>
+    <div v-else>
+	<login-form @login="logMeIn($event)"></login-form>
+    </div>
   </div>
 </template>
 
 <script>
+import LoginForm from "./LoginForm";
 import "milligram";
 export default {
 	data() {
 	  return {
-	    email: 'ppietraszek@student.agh.edu.pl',
 		authenticatedEmail: '',
-	    password: ''
       };
 	},
 	methods: {
-      logMeIn() {
-        this.authenticatedEmail = this.email;
+      logMeIn(username) {
+        this.authenticatedEmail = username;
       },
 	   logMeOut() {
         this.authenticatedEmail = '';
 		this.email = '';
       }
-    }
-	
+    },
+	components: {LoginForm},
 };
 </script>
 
